6231350349a6b6c71c3e80ce790622467233f2b5
brollins <brollins@7267dece-b0b6-9347-80ab-8ffb90f30002>
2014-10-22 18:30:59 +0000

    made my vertex shader processing work
    git-svn-id: https://svn.neumont.edu:8443/svn/gat160blake_rollins@79 7267dece-b0b6-9347-80ab-8ffb90f30002

--- GLProjectBlakeRollins/GLProjectBlakeRollins/Shaders/VertexShaderVertex.glsl
+++ GLProjectBlakeRollins/GLProjectBlakeRollins/Shaders/VertexShaderVertex.glsl
@@ -4,24 +4,38 @@ in layout(location=0) vec4 vertexPositionModel;
 in layout(location=1) vec4 vertexColor;
 in layout(location=2) vec3 vertexNormalModel;
 
-uniform vec4 ambientLightColor;
-uniform vec3 lightPositionWorld;
 uniform mat4 modelToProjectionMatrix;
 uniform mat4 modelToWorldMatrix;
+uniform vec4 ambientLightColor;
+uniform vec4 diffuseLightColor;
+uniform vec4 specularLightColor;
+uniform vec3 lightPositionWorld;
+uniform vec3 cameraPositionWorld;
+uniform float specularExponent;
 
 out vec4 fragmentColor;
 
 void main()
 {
     gl_Position = modelToProjectionMatrix * vertexPositionModel;
-
-	vec3 trans_normal = mat3(modelToWorldMatrix) * vertexNormalModel;
-	vec3 trans_position = vec3(modelToWorldMatrix * vertexPositionModel);
-
-	vec3 lightVector = normalize(lightPositionWorld - trans_position);
-	float brightness = dot(lightVector, normalize(trans_normal));
+	
+	vec3 vertexPositionWorld = vec3(modelToWorldMatrix * vertexPositionModel);
+	vec3 vertexNormalWorld = mat3(modelToWorldMatrix) * vertexNormalModel;
+	
+	// Diffuse light
+	vec3 lightVectorWorld = normalize(lightPositionWorld - vertexPositionWorld);
+	float brightness = dot(lightVectorWorld, normalize(vertexNormalWorld));
     vec4 diffuseLightIntensity = vec4(brightness, brightness, brightness, 1.0);
+	vec4 diffuseColor = diffuseLightIntensity * diffuseLightColor;
 
-	fragmentColor = clamp(diffuseLightIntensity, 0, 1) + ambientLightColor;
+	// Specular light
+	vec3 reflectedLightVectorWorld = reflect(-lightVectorWorld, normalize(vertexNormalModel));
+	vec3 eyeVectorWorld = normalize(cameraPositionWorld - vertexPositionWorld);
+	float specularity = dot(reflectedLightVectorWorld, eyeVectorWorld);
+	specularity = pow(specularity, specularExponent);
+	
+	vec4 specularLightIntensity = vec4(specularity, specularity, specularity, 1.0);
+	vec4 specularColor = specularLightIntensity * specularLightColor;
 
+	fragmentColor = ambientLightColor + clamp(diffuseColor, 0, 1) + clamp(specularColor, 0, 1);
 }
